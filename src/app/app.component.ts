530938d45ee832f4dda773c03e8217252291a81d
Johan Aspeling <aspeling.johan@gmail.com>
2021-06-10 21:54:44 +0200

    chore: positioned the players on top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, ChangeDetectorRef, Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'positioning';
+export class AppComponent implements OnInit, AfterContentInit {
+
+    players: string[] = [
+        'assets/images/keeper.png',
+        'assets/images/batsman1.png',
+        'assets/images/batsman2.png',
+        'assets/images/bowler.png',
+        'assets/images/cover.png',
+        'assets/images/legside.png',
+        'assets/images/point.png',
+        'assets/images/receiver.png',
+        'assets/images/sweeper_left.png',
+        'assets/images/sweeper_right.png',
+    ]
+
+    constructor(private readonly cdr: ChangeDetectorRef) {
+    }
+
+    public ngOnInit(): void {
+        this.cdr.detectChanges()
+
+    }
+
+    ngAfterViewInit(): void {
+        this.cdr.detectChanges()
+    }
+
+    ngAfterContentInit(): void {
+        this.cdr.detectChanges()
+    }
+
+    calculateWidth(field: HTMLImageElement): number {
+        return field.offsetWidth / 8;
+    }
 }
