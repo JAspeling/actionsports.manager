d23071a6463c962dd0be2c3315a810d342cc1bd3
Johan Aspeling <aspeling.johan@gmail.com>
2021-06-13 15:17:40 +0200

    chore: made the players' height set according to the field size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
-import { AfterContentInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import {
+    AfterContentInit,
+    ChangeDetectorRef,
+    Component,
+    ElementRef,
+    OnInit,
+    ViewChild,
+    ViewChildren
+} from '@angular/core';
 import { Player } from "./models/player";
-import { Props } from "./models/prop";
+import { Prop, Props } from "./models/prop";
 
 @Component({
     selector: 'app-root',
@@ -8,17 +16,10 @@ import { Props } from "./models/prop";
 })
 export class AppComponent implements OnInit, AfterContentInit {
 
+    @ViewChild('field') field: ElementRef<HTMLImageElement>;
+    fieldPos: { x: number, y: number, height: number } = { x: 0, y: 0, height: 0 };
+
     players: Props = [
-        new Player({ source: 'assets/svg/receiver.svg' }),
-        new Player({ source: 'assets/svg/sweeper_left.svg' }),
-        new Player({ source: 'assets/svg/sweeper_right.svg' }),
-        new Player({ source: 'assets/svg/cover.svg' }),
-        new Player({ source: 'assets/svg/legside.svg' }),
-        new Player({ source: 'assets/svg/point.svg' }),
-        new Player({ source: 'assets/svg/keeper.svg' }),
-        new Player({ source: 'assets/svg/bowler.svg' }),
-        new Player({ source: 'assets/svg/batsman1.svg' }),
-        new Player({ source: 'assets/svg/batsman2.svg' }),
         // new Prop({source: 'assets/images/keeper.png'}),
         // new Prop({source: 'assets/images/keeper.png'}),
     ]
@@ -32,14 +33,54 @@ export class AppComponent implements OnInit, AfterContentInit {
     }
 
     ngAfterViewInit(): void {
-        this.cdr.detectChanges()
+        this.cdr.detectChanges();
+
+        const fieldRect = this.field.nativeElement.getBoundingClientRect();
+        console.log('Field Rect', fieldRect);
     }
 
     ngAfterContentInit(): void {
-        this.cdr.detectChanges()
+        this.cdr.detectChanges();
     }
 
     calculateWidth(field: HTMLImageElement): number {
         return field.offsetWidth / 8;
+    }
+
+    log(event): void {
+        // console.log(event);
+        const target = event.source.element.nativeElement;
+        const rect = target.getBoundingClientRect();
+        console.log(`${rect.left}, ${rect.top}`);
+    }
+
+    fieldLoaded(event: Event) {
+        const img = event.composedPath()[0] as HTMLImageElement;
+
+        this.fieldPos.x = img.x;
+        this.fieldPos.y = img.y;
+        this.fieldPos.height = img.height;
+
+        this.players = [
+            new Player({ source: 'assets/svg/receiver.svg', baseHeight: 276 }),
+            new Player({ source: 'assets/svg/sweeper_left.svg', baseHeight: 276 }),
+            new Player({ source: 'assets/svg/sweeper_right.svg', baseHeight: 306 }),
+            new Player({ source: 'assets/svg/cover.svg', baseHeight: 216 }),
+            new Player({ source: 'assets/svg/legside.svg', baseHeight: 200 }),
+            new Player({ source: 'assets/svg/point.svg', baseHeight: 211 }),
+            new Player({ source: 'assets/svg/keeper.svg', baseHeight: 221 }),
+            new Player({ source: 'assets/svg/bowler.svg', baseHeight: 395 }),
+            new Player({ source: 'assets/svg/batsman1.svg', baseHeight: 304 }),
+            new Player({ source: 'assets/svg/batsman2.svg', baseHeight: 304 }),
+        ]
+
+        console.log(`Image loaded`, this.fieldPos);
+    }
+
+    playerLoaded(event: Event, player: Prop): void {
+        const img = event.currentTarget as HTMLImageElement;
+        img.height = player.getInitialHeight(this.fieldPos.height);
+
+        console.log(img.height);
     }
 }
--- src/app/models/prop.ts
+++ src/app/models/prop.ts
@@ -10,7 +10,23 @@ export class Prop {
     y: number;
     zIndex: number;
 
+    // Height that is in proportion with the middle of the pitch.
+    baseHeight: number;
+
+    // The (base) ratio to use when moving up/down the pitch.
+    ratio: number = 1.28;
+
+    // from designs
+    originalFieldHeight: number = 2368;
+
     constructor(init?: Partial<Prop>) {
         Object.assign(this, init);
     }
+
+    getInitialHeight(fieldHeight: number): number {
+        const fieldRatio = fieldHeight / this.originalFieldHeight;
+
+        return this.baseHeight * fieldRatio;
+    }
 }
+
