b331812393f45c7de318f1b8bc771a4c9f71a144
Johan Aspeling <aspeling.johan@gmail.com>
2021-06-13 20:26:36 +0200

    chore: added stumps chore: predetermined positions chore: ratio resize on props

--- src/app/models/prop.ts
+++ src/app/models/prop.ts
@@ -1,17 +1,30 @@
 export type Props = Prop[];
+
 export class Prop {
     source: string;
+    element: HTMLImageElement;
 
     // Original 'height' to use as a resize basis
     originalRatio: number;
     height: number;
     width: number;
+
+    // initial positions, percentage-based from the field ratio
+    initial: {
+        x: number,
+        y: number;
+    } = { x: 0, y: 0 }
+
     x: number;
     y: number;
     zIndex: number;
 
     // Height that is in proportion with the middle of the pitch.
     baseHeight: number;
+    initialBaseHeight: number;
+
+    // The rendered height given all the other variables.
+    actualHeight: number;
 
     // The (base) ratio to use when moving up/down the pitch.
     ratio: number = 1.28;
@@ -25,8 +38,19 @@ export class Prop {
 
     getInitialHeight(fieldHeight: number): number {
         const fieldRatio = fieldHeight / this.originalFieldHeight;
+        this.initialBaseHeight = this.baseHeight * fieldRatio;
 
-        return this.baseHeight * fieldRatio;
+        return this.initialBaseHeight;
+    }
+
+    setInitialPosition(fieldElement: HTMLImageElement): void {
+        const parentTop: number = fieldElement.offsetTop;
+        const parentLeft: number = fieldElement.offsetLeft;
+        const fieldHeight = fieldElement.offsetHeight;
+        const fieldWidth = fieldElement.offsetWidth;
+
+        this.x = (fieldWidth * this.initial.x / 100) + parentLeft;
+        this.y = (fieldHeight * this.initial.y / 100) + parentTop;
     }
 }
 
